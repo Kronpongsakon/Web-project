f64ba88320f375cbba0a88e401348fed76ec5cd8
LittleShy23 <112997517+LittleShy23@users.noreply.github.com>
2024-03-02 22:36:53 +0700

    upgrate

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCube,faAngleRight,faUser } from '@fortawesome/free-solid-svg-icons'
 import { useState, useEffect } from 'react';
-import { useQuery,useMutation, gql } from "@apollo/client";
+import { useQuery, useMutation, useSubscription, gql } from "@apollo/client";
 
 function App() {
   const GET_TODO_WITH_USER = gql`
@@ -37,20 +37,43 @@ function App() {
     }
   `;
 
-  const { data: listTodos, refetch } = useQuery(GET_TODO_WITH_USER);
+  const DELETE_ALL_TODOS = gql`
+    mutation DeleteAllTodo {
+      delete_todos (where: {}) {
+        affected_rows
+      }
+    }
+  ` ;
+
+  const TODO_SUBSCRIPTION = gql`
+    subscription {
+      todos(order_by: { created_at: desc }) {
+        title
+        user {
+          id
+          name
+        }
+      }
+    }
+  `;
+
+  const { data: listTodos } = useQuery(GET_TODO_WITH_USER);
+  const { data: newListTodos } = useSubscription(TODO_SUBSCRIPTION);
   const { data: listUser } = useQuery(GET_USER);
   const [createTodo] = useMutation(CREATE_TODO);
+  const [deleteAllTodos] = useMutation(DELETE_ALL_TODOS);
   const [todos, setTodos] = useState([]);
   const [comment, setComment] = useState("");
-  const [newTodo, setNewTodo] = useState();
+  const [hasNewTodo, setHasNewTodo] = useState(false);
   const [users, setUser] = useState([]);
+  let item = []
+  const type_item = ["All", "Active", "Completed", "Clear Completed"]
 
   const handleCreateTodo = async () => {
     try {
-      const response = await createTodo({
+      await createTodo({
         variables: { user_id: 1, title: comment },
       });
-      await setNewTodo(response.data.insert_todos.returning)
     } catch (error) {
       console.error("Error creating todo:", error);
     }
@@ -68,11 +91,34 @@ function App() {
 
   const handleSubmit = () => {
     handleCreateTodo();
+    var newTodos = [...todos]
+    newTodos.unshift({title: comment, user: {name: "Kronpongsakon Kronkum"}})
+    setTodos(newTodos)
     setComment("");
   };
 
+  const handleDeleteTodo = () => {
+    deleteAllTodos()
+      .then(() => {setTodos([])})
+      .catch(error => {
+        console.error('Error deleting all todos:', error);
+      });
+  };
+
+  const handleNewTodo = () => {
+    setHasNewTodo(false);
+    const maxHightTodoDiv = document.querySelector('.max-hight-todo');
+    if (maxHightTodoDiv) {
+     maxHightTodoDiv.scrollTo({
+       top: 0,
+       behavior: 'smooth'
+     });
+   }
+   setTodos(newListTodos.todos)
+ }
+
   useEffect(() => {
-    if (listTodos) {
+    if (listTodos && listTodos.todos) {
       setTodos(listTodos.todos);
     }
   }, [listTodos]);
@@ -83,13 +129,14 @@ function App() {
     }
   }, [listUser]);
 
-  const handleNewTodo = () => {
-     refetch();
-     setNewTodo();
-  }
+  useEffect(() => {
+    if (newListTodos && newListTodos.todos) {
+      if (todos.length != newListTodos.todos.length){
+        setHasNewTodo(true)
+      }
+    }
+  }, [newListTodos]);
 
-  let item = []
-  const type_item = ["All", "Active", "Completed", "Clear Completed"]
   return (
     <div>
       <div className='logout-box'>
@@ -102,13 +149,13 @@ function App() {
           </div>
           <div className="col-6 px-0">
             <div className="d-flex justify-content-end">
-              <button className="button-box logout-text mt1">Log out</button>
+              <button className="button-box logout-button mt1">Log out</button>
             </div>
           </div>
         </div>
       </div>
 
-      <div className="row mt-2 me-0 max-hight-todo">
+      <div className="row mt-2 me-0">
         <div className="col-4 background-orange">
           <div className="p-2">
             <div className="text-size fw-bold border-bottom-red">
@@ -123,8 +170,8 @@ function App() {
             <div className="background-white box border-radius-input mt-2 p-2">
               <div className="d-flex align-items-center">
                   <div className='px-2 mt-1'>{item.length} item</div>
-                  <div>{type_item.map((type) => (
-                    <button class="ms-1 button-box-type fw-bold mt-1">{type}</button>
+                  <div>{type_item.map((type, index) => (
+                    <button key={index} className="ms-1 button-box-type fw-bold mt-1">{type}</button>
                   ))}</div>
               </div>
             </div>
@@ -133,8 +180,13 @@ function App() {
 
         <div className="col-4 background-green">
           <div className="p-2">
-            <div className="text-size fw-bold border-bottom-red">
-              Public feed (realtime)
+            <div className="row border-bottom-red">
+              <div className="col-8 text-size fw-bold">
+                Public feed (realtime)
+              </div>
+              <div className="col-4 d-flex justify-content-end align-items-center">
+                <button className="button-box delete-button mt1" onClick={handleDeleteTodo}>Delete All Todo</button>
+              </div>
             </div>
             <div className="d-flex mt-2">
               <div className='px-2 pt-3 position-absolute'>
@@ -148,15 +200,15 @@ function App() {
                 onKeyPress={handleKeypressInput}
               />
             </div>
-            { newTodo ? (<div className='d-flex justify-content-center mt-2'>
-                <button className='button-box-type' onClick={handleNewTodo}>Have new Todo</button>
-            </div>) : ""}
-            <div>
-              {todos.map((todo) => (
-              <div className="background-white box border-radius-input mt-2 p-2">
+            { hasNewTodo ? (<div className='d-flex justify-content-center'>
+                  <button className='button-box-type mt-1 position-absolute' onClick={handleNewTodo}>Have new Todo</button>
+              </div>) : null}
+            <div className="max-hight-todo">  
+              {todos.map((todo, index) => (
+              <div key={index} className="background-white box border-radius-input mt-2 p-2">
                 <div className="d-flex align-items-center">
-                  <div class="nameUser fw-bold p-1">@{todo.user.name}</div>
-                  <div class="ms-2">{todo.title}</div>
+                  <div className="nameUser fw-bold p-1">@{todo.user.name}</div>
+                  <div className="ms-2">{todo.title}</div>
                 </div>
               </div>
               ))}   
@@ -168,12 +220,12 @@ function App() {
         <div className="col-4 background-yellow">
           <div className="p-2">
             <div className="text-size fw-bold border-bottom-red mb-user">Online_user - {users.length}</div>
-            <div>{users.map((user) => (
-              <div className="d-flex align-items-center pt-2">
+            <div>{users.map((user, index) => (
+              <div key={index} className="d-flex align-items-center pt-2">
                 <div className='pt-3 mb-1'>
                   <FontAwesomeIcon icon={faUser} />
                 </div>
-                <div class="text-size text-bold ps-2 pt-2">{user.name}</div>
+                <div className="text-size text-bold ps-2 pt-2">{user.name}</div>
               </div>
             ))}</div>
           </div>
--- src/index.js
+++ src/index.js
@@ -3,10 +3,35 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { ApolloProvider } from '@apollo/client';
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, HttpLink, split } from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from '@apollo/client/utilities';
+
+const httpLink = new HttpLink({
+  uri: 'https://delicate-piglet-42.hasura.app/v1/graphql',
+});
+
+const wsLink = new WebSocketLink({
+  uri: `wss://delicate-piglet-42.hasura.app/v1/graphql`,
+  options: {
+    reconnect: true,
+  },
+});
+
+const link = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
 
 const client = new ApolloClient({
-  uri: 'https://delicate-piglet-42.hasura.app/v1/graphql',
+  link,
   cache: new InMemoryCache(),
 });
 
@@ -16,5 +41,3 @@ root.render(
     <App />
   </ApolloProvider>,
 );
-
-
